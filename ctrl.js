fe405e5d02b7b96a2daf4089c832d6d8489abd3e
SheaClose <SheaClose@gmail.com>
2017-09-18 12:09:06 -0500

    done

--- ctrl.js
+++ ctrl.js
@@ -1,14 +1,1 @@
-angular.module('app', []).controller('ctrl', function($scope, $timeout) {
-  var socket = io();
-  $scope.messages = [];
-
-  $scope.submit = function(inputMessage, userName) {
-    socket.emit('chat message', inputMessage, userName);
-    $scope.inputMessage = '';
-  };
-  socket.on('chat message', function(msg, user) {
-    $timeout(function() {
-      $scope.messages.push({ msg, user });
-    });
-  });
-});
+angular.module('app', []).controller('ctrl', function($scope) {});
